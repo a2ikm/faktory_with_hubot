4b63a17e32566dfe330d096363c47172e8cd5f95
Masato Ikeda <masato.ikeda@gmail.com>
2018-03-10 23:16:57 +0900

    worker: reply in slack

--- worker/app.rb
+++ worker/app.rb
@@ -1,10 +1,18 @@
-require "pp"
 require "dotenv/load"
+require "slack-ruby-client"
+
+Slack.configure do |config|
+  config.token = ENV["SLACK_API_TOKEN"]
+end
+
+SLACK = Slack::Web::Client.new
+SLACK.auth_test
 
 class Hello
   include Faktory::Job
 
-  def perform(*args)
-    logger.info args.pretty_inspect
+  def perform(user, text, room)
+    text = "@#{user["name"]} Hi"
+    SLACK.chat_postMessage(channel: room, text: text)
   end
 end
