4bd4e6839b422d97acf172a3dd8a44f8eda81d31
Masato Ikeda <masato.ikeda@gmail.com>
2018-03-10 23:54:47 +0900

    worker: link mentions

--- worker/app.rb
+++ worker/app.rb
@@ -15,6 +15,6 @@ class Hello
     user = message["user"]["name"]
     channel = message["room"]
     text = "@#{user} Hi"
-    SLACK.chat_postMessage(channel: channel, text: text)
+    SLACK.chat_postMessage(channel: channel, text: text, link_names: true)
   end
 end
