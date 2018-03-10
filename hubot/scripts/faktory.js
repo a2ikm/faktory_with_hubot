15af68276dcd312b48ccb88a5e6e82a4ffd70865
Masato Ikeda <masato.ikeda@gmail.com>
2018-03-10 21:38:47 +0900

    hubot: enqueue to faktory

--- hubot/scripts/faktory.js
+++ hubot/scripts/faktory.js
@@ -1,5 +1,14 @@
+const Faktory = require("faktory-client")
+const faktory = new Faktory()
+faktory.connect()
+
 module.exports = (robot => {
-  robot.hear(/hello/i, res => {
-    res.send("Hi")
+  robot.hear(/hello/i, async res => {
+    jid = await faktory.push({
+      jobtype: "Hello",
+      queue: "hello",
+      args: [res.message.user, res.message.text]
+    })
+    res.send(`jid = ${jid}`)
   })
 })
